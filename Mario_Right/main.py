64d6953e9583e15f1ee9dc4ea07644eabdff4d17
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2021-11-27 15:13:57 -0800

    latest checkpoints

--- Mario_Right/main.py
+++ Mario_Right/main.py
@@ -35,7 +35,7 @@ env.reset()
 save_dir = Path('checkpoints_train') / datetime.datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
 save_dir.mkdir(parents=True)
 
-checkpoint = Path('checkpoints_train/2021-11-25T12-27-45/mario.chkpt')
+checkpoint = Path('checkpoints_train/2021-11-26T08-14-10/mario.chkpt')
 # checkpoint = None
 mario = Mario(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir, checkpoint=checkpoint)
 
